0097350112208048c93ff4af35edebb9599b5bd4
= <aymancoom3@gmail.com>
2023-11-05 12:34:36 +0200

    B

--- app/Http/Controllers/QueryOfReportController.php
+++ app/Http/Controllers/QueryOfReportController.php
@@ -6,6 +6,7 @@ use App\Models\QueryOfReport;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use STS\JWT;
 use STS\JWT\JWTFacade;
 // use STS\JWT\JWTFacade;
 
